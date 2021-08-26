4d7a407a43c89b475762f568e250440480dd708f
Milagros Lechares <80794759+mflec@users.noreply.github.com>
2021-08-26 09:44:07 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db");
 
 const PORT = 3001
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
       console.log(`Listening at ${PORT}...`)
   })
